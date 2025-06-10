9e0ff7eaa04c732229eb006d9e479d23e23dde5b
solemannoushad <solemannoushad31@gmail.com>
2025-06-10 03:14:56 -0700

    gsap.timeline

--- js/lesson1.js
+++ js/lesson1.js
@@ -35,3 +35,28 @@ gsap.to("#box" , {
 })
 
 
+const tl = gsap.timeline();
+
+tl.from(".logo" , {
+    y: -100,
+    duration: 0.5,
+    opacity: 0,
+    ease: "bounce.out",
+})
+
+tl.from(".list-items li" , {
+    y: 30,
+    duration: 0.5,
+    opacity: 0,
+    stagger: 0.2,
+    ease: "back.out(1.7)",
+})
+
+tl.from("h1" ,{
+    y:20,
+    opacity: 0,
+    scale: 0.2,
+    duration: 0.5,
+})
+
+
